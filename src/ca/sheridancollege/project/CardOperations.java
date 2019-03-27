8aae27557762865b536f82781ee187d2120e4872
sudrs <sudrs@Sud>
2019-03-27 19:28:07 -0400

    updated classes

---
+++ src/ca/sheridancollege/project/CardOperations.java
@@ -0,0 +1,30 @@
+package ca.sheridancollege.project;
+
+public class CardOperations {
+
+	public void flipCard() {
+		// TODO - implement CardOperations.flipCard
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean compareCard() {
+		// TODO - implement CardOperations.compareCard
+		throw new UnsupportedOperationException();
+	}
+
+	public void updatePile() {
+		// TODO - implement CardOperations.updatePile
+		throw new UnsupportedOperationException();
+	}
+
+	public void declareWar() {
+		// TODO - implement CardOperations.declareWar
+		throw new UnsupportedOperationException();
+	}
+
+	public void displayUserCard() {
+		// TODO - implement CardOperations.displayUserCard
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/ca/sheridancollege/project/WarGame.java
@@ -0,0 +1,14 @@
+package ca.sheridancollege.project;
+
+public class WarGame {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public void main(String[] args) {
+		// TODO - implement WarGame.main
+		throw new UnsupportedOperationException();
+	}
+
+}
