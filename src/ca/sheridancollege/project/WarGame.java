871b3ac66202dbbdae7e19f3ec0daf5267f93e09
sudrs <sudrs@Sud>
2019-04-07 20:06:59 -0400

    Revert "Revert "Updated UML diagram""
    This reverts commit 2b7191b8e434a2e5c9679f405a0f71fd601c567b.

--- src/ca/sheridancollege/project/WarGame.java
+++ src/ca/sheridancollege/project/WarGame.java
@@ -11,8 +11,14 @@ import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -23,6 +29,30 @@ public class WarGame extends Application {
     
     @Override
     public void start(Stage primaryStage) {
+        
+         BorderPane root= new BorderPane();
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setTitle("War Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        root.setStyle("-fx-background-color: #009900");
+        
+       
+        FlowPane table= new FlowPane();
+        root.setCenter(table);
+        table.setAlignment(Pos.CENTER);
+        
+        FlowPane textPanel= new FlowPane();
+        root.setTop(textPanel);
+        textPanel.setAlignment(Pos.CENTER);
+        
+        Text txt=new Text();
+        
+        txt.setText("Welcome to Game");
+        txt.setFill(Color.BLUE);
+        txt.setFont(Font.font(25));
+        textPanel.getChildren().add(txt);
+        
         Button flip = new Button("Filp");
         flip.setOnAction(new EventHandler<ActionEvent>() {
             
@@ -63,22 +93,23 @@ public class WarGame extends Application {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                primaryStage.close();
             }
-        });
+        });      
         FlowPane buttonPanel = new FlowPane();
         buttonPanel.setAlignment(Pos.CENTER);
-        buttonPanel.getChildren().addAll(flip,shuffle,displayCard,reset,quit);
         root.setBottom(buttonPanel);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("War Game");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        
-        
+        buttonPanel.getChildren().addAll(flip,shuffle,displayCard,reset,quit);
+    
+       Image imgA= new Image("images/cardback.png");
+       ImageView ivA= new ImageView(imgA);
+       table.getChildren().add(ivA);
+       ivA.setPreserveRatio(true);
+       ivA.setFitWidth(100);
+       
     }
+    
+
 
     /**
      * @param args the command line arguments
