28db276f4fe312d33818f10b73488bd75b61b769
sudrs <sudrs@Sud>
2019-04-09 23:22:06 -0400

    new Updates: Created Display Cards, InitCards

--- src/ca/sheridancollege/project/WarGame.java
+++ src/ca/sheridancollege/project/WarGame.java
@@ -5,12 +5,16 @@
  */
 package ca.sheridancollege.project;
 
+import java.util.Optional;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -27,38 +31,66 @@ import javafx.stage.Stage;
  */
 public class WarGame extends Application {
     
+    
+    Text txt=new Text();
+    Text txt2=new Text();
+    Text txt3=new Text();
+    FlowPane textPanel= new FlowPane();
+    FlowPane table= new FlowPane();
+    GroupOfCards cg=new GroupOfCards();
+    
+    
     @Override
     public void start(Stage primaryStage) {
         
-         BorderPane root= new BorderPane();
-        Scene scene = new Scene(root, 800, 600);
+        BorderPane root= new BorderPane();
+        Scene scene = new Scene(root, 900, 650);
         primaryStage.setTitle("War Game");
         primaryStage.setScene(scene);
         primaryStage.show();
-        root.setStyle("-fx-background-color: #009900");
+        root.setStyle("-fx-background-color: green");
         
        
-        FlowPane table= new FlowPane();
+       
         root.setCenter(table);
         table.setAlignment(Pos.CENTER);
         
-        FlowPane textPanel= new FlowPane();
+        FlowPane playerPanel=new FlowPane();
+        root.setLeft(playerPanel);
+        playerPanel.setAlignment(Pos.TOP_CENTER);
+        
+        FlowPane computerPanel=new FlowPane();
+        root.setRight(computerPanel);
+        computerPanel.setAlignment(Pos.BOTTOM_CENTER);
+        
+        
         root.setTop(textPanel);
         textPanel.setAlignment(Pos.CENTER);
         
-        Text txt=new Text();
+        txt2.setText("Computer");
+        txt3.setText("Player");
         
-        txt.setText("Welcome to Game");
-        txt.setFill(Color.BLUE);
-        txt.setFont(Font.font(25));
-        textPanel.getChildren().add(txt);
+        Alert welcome= new Alert(AlertType.INFORMATION);
+        welcome.setTitle("Welcome");
+        welcome.setHeaderText(null);
+        welcome.setContentText("Welcome to War Game");
+        welcome.showAndWait();
+        
+        callTextMain("Good Luck");
         
         Button flip = new Button("Filp");
+        cg.initCards();
+        cg.shuffle();
+        cg.distributeCards();
+        cg.shuffle();
+        
+        
         flip.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+              flipPlayerCard();
+              flipComputerCard();
             }
         });
         
@@ -67,7 +99,7 @@ public class WarGame extends Application {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                cg.shuffle();
             }
         });
         
@@ -84,7 +116,8 @@ public class WarGame extends Application {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                //first();
+                
             }
         });
         
@@ -93,24 +126,80 @@ public class WarGame extends Application {
             
             @Override
             public void handle(ActionEvent event) {
+                
+                Alert quit= new Alert(AlertType.CONFIRMATION);
+                    quit.setTitle("Quit");
+                    quit.setHeaderText(null);
+                    quit.setContentText("Do you want to quit?");
+                    
+                Optional<ButtonType> result=quit.showAndWait();
+                
+                if(result.get()==ButtonType.OK){
                 primaryStage.close();
-            }
+            }}
         });      
-        FlowPane buttonPanel = new FlowPane();
-        buttonPanel.setAlignment(Pos.CENTER);
-        root.setBottom(buttonPanel);
-        buttonPanel.getChildren().addAll(flip,shuffle,displayCard,reset,quit);
+        
+       FlowPane buttonPanel = new FlowPane();
+       buttonPanel.setAlignment(Pos.CENTER);
+       root.setBottom(buttonPanel);
+       buttonPanel.getChildren().addAll(flip,shuffle,displayCard,reset,quit);
     
        Image imgA= new Image("images/cardback.png");
        ImageView ivA= new ImageView(imgA);
-       table.getChildren().add(ivA);
+       playerPanel.getChildren().addAll(ivA,txt3);
        ivA.setPreserveRatio(true);
        ivA.setFitWidth(100);
        
+       Image imgB= new Image("images/cardback.png");
+       ImageView ivB= new ImageView(imgB);
+       computerPanel.getChildren().addAll(ivB,txt2);
+       ivB.setPreserveRatio(true);
+       ivB.setFitWidth(100);
+       
+       
+        
+    }
+
+    private void callTextMain(String message) {
+        txt.setText(message);
+        txt.setFill(Color.BLUE);
+        txt.setFont(Font.font(20));
+        textPanel.getChildren().add(txt);
     }
     
+//public void first(){
+//     
+//       
+//       cg.initCards();
+//       
+//       for(Card a: cg.deck){
+//           System.out.println(a.getValue()+"of"+a.getSuit());
+//       }
+//       
+//       cg.distributeCards();
+//    }
 
+public void flipComputerCard() {
+	Card c = cg.computerPile.remove(0);
+	cg.tablePile.add(c);
+        DisplayCard(c,table);
+	}
 
+public void flipPlayerCard() {
+		Card c = cg.playerPile.remove(0);
+		cg.playerPile.add(c);
+		DisplayCard(c, table);
+
+	}
+public void DisplayCard(Card c, FlowPane panel) {
+     
+		Image img1 = new Image("images/"+c.GetFileName());
+		ImageView iv1 = new ImageView(img1);
+		panel.getChildren().add(iv1);
+		iv1.setPreserveRatio(true);
+		iv1.setFitWidth(100);
+
+	}
     /**
      * @param args the command line arguments
      */
